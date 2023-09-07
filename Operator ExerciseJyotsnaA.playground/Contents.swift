c02882ed624c0782c5a183dc7b0a7afc2d66abf7
Agrawal, Jyotsna <jagrawal@students.mchenry.edu>
2023-09-07 12:01:26 -0500

    CalulatorUsingOpertor

---
+++ Operator ExerciseJyotsnaA.playground/Contents.swift
@@ -0,0 +1,23 @@
+import UIKit
+
+// Input Variables
+
+let num1 = 15.0
+let num2 = 5.0
+
+// Define Operations
+let addition = num1 + num2
+let subtraction = num1 - num2
+let multiplication = num1 * num2
+let division = num1 / num2
+let percentage = (num1 / num2) * 100.0
+
+// Display Results
+
+print("Results:")
+print("\(num1) + \(num2) = \(addition)")
+print("\(num1) - \(num2) = \(subtraction)")
+print("\(num1) * \(num2) =\(multiplication)")
+print("\(num1) / \(num2) = \(division)")
+print("\(num1) as a percentage of \(num2) = \(percentage)%")
+
