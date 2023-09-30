dd4588e98b4d970ef43941657d93bb9564fbe632
Agrawal, Jyotsna <jagrawal@students.mchenry.edu>
2023-09-30 14:41:49 -0500

    FibonachchiNumbers 0 to Up to 1000

--- FibonachchiNumberJyotsnaA.playground/Contents.swift
+++ FibonachchiNumberJyotsnaA.playground/Contents.swift
@@ -1,37 +1,25 @@
-// Import the UIKit framework (used for iOS development)
 import UIKit
-
-// Ingredients
-var current = 1 // Initialize the current Fibonacci number to 1
-var previous = 0 // Initialize the previous Fibonacci number to 0
-let n = 10 // Change 'n' to the number of Fibonacci numbers you want to generate
-
-// Function to calculate Fibonacci numbers
-func generateFibonacciNumbers(_ count: Int) {
-    print("Fibonacci Numbers:") // Print a header for the Fibonacci numbers
+// Function to calculate and display Fibonacci numbers up to a given limit
+func calculateAndDisplayFibonacciNumbers(upTo limit: Int) {
+    print("Fibonacci Numbers:")
     
-    // Check if we should print the first Fibonacci number (0)
-    if count >= 1 {
-        print("0")
-    }
+    var current = 1
+    var previous = 0
     
-    // Check if we should print the second Fibonacci number (1)
-    if count >= 2 {
-        print("1")
-    }
+    // Handle the first two Fibonacci numbers (0 and 1)
+    print("0")
+    print("1")
     
-    // Generate the rest of the Fibonacci numbers
-    if count >= 3 { // We start generating from the third Fibonacci number onwards
-        for _ in 3...count {
-            let next = current + previous // Calculate the next Fibonacci number
-            print(next) // Print the next Fibonacci number
-            
-            // Update 'previous' and 'current' for the next iteration
-            previous = current
-            current = next
-        }
+    // Generate Fibonacci numbers up to the given limit
+    while current + previous <= limit {
+        let next = current + previous
+        print(next)
+        
+        // Update 'previous' and 'current' for the next iteration
+        previous = current
+        current = next
     }
 }
 
-// Generate and print Fibonacci numbers
-generateFibonacciNumbers(n) // Call the function with 'n' as the count of Fibonacci numbers to generate
+// Call the function to calculate and display Fibonacci numbers up to 1000
+calculateAndDisplayFibonacciNumbers(upTo: 1000)
